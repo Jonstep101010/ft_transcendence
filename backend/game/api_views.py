98c0500507fbc4498686cdbb773f57a6d03def34
jsanger <jani.saenger@gmail.com>
2024-11-29 04:22:50 +0100

    added API for gameControl

--- backend/game/api_views.py
+++ backend/game/api_views.py
@@ -33,7 +33,61 @@ class CreateGameView(APIView):
 		opponent_profile = Profile.objects.get(user=opponent)
 
 		# Create the game
-		game = Game.objects.create(player1=user_profile, player2=opponent_profile)
+		game = Game.objects.create(player1=user_profile.player, player2=opponent_profile.player)
 		game.save()
 
 		return Response({"game_id": game.id, "message": "Game created successfully."}, status=status.HTTP_201_CREATED)
+
+class ScoreBoardView(APIView):
+	permission_classes = [IsAuthenticated]
+
+	def post(self, request):
+		game_id = request.data.get('game_id')
+		score1 = request.data.get('score1')
+		score2 = request.data.get('score2')
+		if not game_id or not score1 or not score2:
+			return Response({"error": "game_id score1 and score2 are requirded"}, status=status.HTTP_400_BAD_REQUEST)
+		
+		try:
+			game = Game.objects.get(id=game_id)
+		except Game.DoesNotExist:
+			return Response({"error": "Game not found."}, status=status.HTTP_404_NOT_FOUND)
+		
+		game = Game.objects.get(id=game_id)
+		game.score1 = int(score1)
+		game.score2 = int(score2)
+		game.save()
+
+		return Response({"scores": "Game successfully saved score."}, status=status.HTTP_200_OK)
+	
+
+class ControllKeySetting(APIView):
+	permission_classes = [IsAuthenticated]
+
+	def post(self, request):
+		game_id = request.data.get('game_id')
+		user = request.user.username
+		control1 = request.data.get('control1')
+		control2 = request.data.get('control2')
+
+		if not game_id or control1 is None or control2 is None:
+			return Response({"error": "game_id, control1, and control2 are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+		try:
+			game = Game.objects.get(id=game_id)
+		except Game.DoesNotExist:
+			return Response({"error": "Game not found."}, status=status.HTTP_404_NOT_FOUND)
+
+		if user == game.player1.__str__():
+			game.player1_control_settings = control1
+		elif user == game.player2.__str__():
+			game.player2_control_settings = control2
+		else:
+			return Response({"error": "You are not a player in this game."}, status=status.HTTP_403_FORBIDDEN)
+
+		game.save()
+
+		return Response(
+			{"message": f"Control settings successfully updated for user {user}."},
+			status=status.HTTP_200_OK
+		)
--- backend/game/urls.py
+++ backend/game/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .api_views import CreateGameView
+from .api_views import CreateGameView, ScoreBoardView, ControllKeySetting
 
 app_name = "game"
 urlpatterns = [
@@ -10,8 +10,8 @@ urlpatterns = [
 	path("players/<int:player_id>/", views.player_details_by_id, name="player_details_by_id"),
 
 	path('api/create-game/', CreateGameView.as_view(), name='api-create-game'),
-	# path('api/get-score/', ScoreBoardView.as_view(), name='api-get-score'),
-	# path('api/get-gameControl/', ControllKeySetting.as_view(), name='api-get-gameControl'),
+	path('api/get-score/', ScoreBoardView.as_view(), name='api-get-score'),
+	path('api/get-gameControl/', ControllKeySetting.as_view(), name='api-get-gameControl'),
 
 	path("new/<int:game_id>/", views.start_game, name="new_game"),
 	path("dashboard/", views.dashboard, name="dashboard"),
